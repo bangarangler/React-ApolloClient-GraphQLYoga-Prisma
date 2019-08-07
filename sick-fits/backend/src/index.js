626685f7d3cc57a5d9f624677c10b29bdc8d5659
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-07 18:46:48 -0400

    Added graphql-yoga and started up server in index.js...
    also needed a dummy query and mutation as we told it we would require it

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO USE EXPRESS MIDDLEWARE TO HANDLE COOKIES JWT
+// TODO USE EXPRESS MIDDLEWARE TO POPULATE CURRENT USER
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  },
+}, deets => {
+  console.log(`Server is now running on port http://localhost:${deets.port}`)
+})
