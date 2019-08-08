ca68a4e0740d9ce21116eb804b2542ffecf5709d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-07 22:19:28 -0400

    Added mutation and query
    createItem and query all items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,16 @@
-const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: CHECK IF LOGGED IN
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
   }
 };
 
-module.exports = mutations;
+module.exports = Mutations;
