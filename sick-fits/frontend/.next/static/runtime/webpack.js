ee15ba71da2a0cf427fdf91f90bcdf115997742a
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-08 15:03:40 -0400

    added update functionallity

--- sick-fits/frontend/.next/static/runtime/webpack.js
+++ sick-fits/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0fbce2f75eded7b920c3";
+/******/ 	var hotCurrentHash = "3a07918a79e3ab5d96de";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
