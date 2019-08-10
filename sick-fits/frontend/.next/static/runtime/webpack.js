603f60bca74682362f69a5a3003322447b54b354
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-10 13:33:37 -0400

    Added Relation to Item...
    user that creates item is linked to that item now

--- sick-fits/frontend/.next/static/runtime/webpack.js
+++ sick-fits/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f1ceb2928445aaaa052a";
+/******/ 	var hotCurrentHash = "e209275758a04e9a203b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
